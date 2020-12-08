a53b6e9f4e2e2a82561e718943ee5fef77077d6c
sd576 <sd576@btinternet.com>
2020-12-08 23:03:15 +0000

    Amended typo in views.py

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,6 +6,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 
 from profiles_api import serializers
 from profiles_api import models
@@ -18,14 +19,14 @@ class HelloApiView(APIView):
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
-        an_apiview =[
-            'Uses HTTP methods function(get, post, patch, put, delete)'
-            'Is similar to a traditional django View',
-            'Gives you the most control over your application logic',
+        an_apiview = [
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over you application logic',
             'Is mapped manually to URLs',
         ]
 
-        return Response({'message': 'Hello', 'an_apiview': an_apiview})
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
 
     def post(self, request):
         """Create a hello message with our name"""
@@ -54,16 +55,15 @@ class HelloApiView(APIView):
         return Response({'method': 'DELETE'})
 
 
-class HellowViewSet(viewsets.ViewSet):
+class HelloViewSet(viewsets.ViewSet):
     """Test API ViewSet"""
     serializer_class = serializers.HelloSerializer
 
     def list(self, request):
         """Return a hello message"""
-
         a_viewset = [
-            'Uses actions (list, create, retrieve, update, partial_update)',
-            'Automatically maps to URLs using routers',
+            'Uses actions (list, create,retrieve, update, partial_update)',
+            'Automatically maps to URLs using Routers',
             'Provides more functionality with less code',
         ]
 
@@ -113,3 +113,15 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 class UserLoginApiView(ObtainAuthToken):
     """Handle creating user authentication tokens"""
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.UpdateOwnStatus, IsAuthenticated)
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
